8514cd4e49a85bfdc4357f97c11507bd6cd11188
Abdallah Khalfan <108978947+heisabdallah@users.noreply.github.com>
2023-03-06 23:51:28 +0300

    Modifications

--- pages/contact.js
+++ pages/contact.js
@@ -57,7 +57,6 @@ const Contact = () => {
                 </header>
                 <footer className={styles.footer} onClick={handleCloseNav}>
                     <div className={styles.contact_section} id="content">
-                        <h1 className={styles.cont_title}>CONTACT</h1>
                         <div className={styles.form}>
                             <h1>Reach To Us</h1>
                             <form action="/feedback">
