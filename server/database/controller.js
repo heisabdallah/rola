ded2c1df9b8ac614ee5c17c326bdcf1c6c6f8c6d
Abdallah Khalfan <108978947+heisabdallah@users.noreply.github.com>
2023-03-12 00:22:33 +0300

    Update controller.js

--- server/database/controller.js
+++ server/database/controller.js
@@ -1,11 +1,12 @@
 
+import { redirect } from "next/dist/server/api-utils"
 import Product from "../model/productModel"
 
 // GET 
 const getProducts = async (req, res) => {
     try{
         const products = await Product.find({})
-        res.status(200).json()
+        res.status(200).json(products)
 
     }catch(error) {
         res.status(404).json({ error: "Products not found" })
@@ -30,6 +31,7 @@ const postProducts = async (req, res) => {
       const formData = req.body;
       const product = await Product.create(formData);
       res.redirect('/about')
+    //   return res.status(200).json(product);
 
     } catch (error) {
       return res.status(404).json({ error });
@@ -44,7 +46,7 @@ const updateProduct = async (req, res) => {
 
         if(productId && formData){
             const product = await Product.findByIdAndUpdate(productId, formData)
-            // res.status(200).json(product)
+            res.status(200).json(product)
         }
 
     } catch (error) {
@@ -57,7 +59,7 @@ const deleteProduct = async (req, res) => {
     try {
         const { productId } = req.query
         const product = await Product.findByIdAndDelete(productId)
-        // res.status(200).json({ deleted: productId })
+        res.status(200).json({ deleted: productId })
 
     } catch (error) {
         res.status(404).json({error: "Error while Deleting Product!"})
