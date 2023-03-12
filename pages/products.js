61ade64413df99fce5399ff2696100d68638eb33
Abdallah Khalfan <108978947+heisabdallah@users.noreply.github.com>
2023-03-12 23:25:38 +0300

    Update products.js

--- pages/products.js
+++ pages/products.js
@@ -1,6 +1,5 @@
 import styles from "../styles/pages/products.module.scss"
 import formStyle from "../styles/components/productForm.module.scss"
-import image1 from "public/images/rola1.jpg"
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
@@ -61,12 +60,6 @@ const Products = () => {
                 </header>
                 <main className={styles.main} onClick={handleCloseNav}>
                     <h1>Products PAGE</h1>
-                    <p>Sunt velit et mollit esse enim do cillum consectetur dolore excepteur magna. Nisi tempor minim anim ea magna ad voluptate in. Amet deserunt magna incididunt consectetur. Eu qui culpa sit minim dolor esse consequat.</p>
-                    <Image
-                        src={image1}
-                    />
-                    <p>Eiusmod eu nulla ad duis veniam mollit commodo commodo dolor. Id cillum occaecat voluptate ipsum et laboris proident occaecat occaecat. Quis aliqua veniam aute duis qui velit ea aute laborum. Tempor consectetur mollit aliqua excepteur occaecat voluptate aliquip exercitation eu enim. Occaecat qui proident excepteur sunt laborum minim consequat. Consequat aliqua officia deserunt quis commodo proident aliqua enim culpa aute. Fugiat cillum deserunt aliqua fugiat dolor elit commodo.</p>
-                    
                     <ProductsComp />
                     <div className={formStyle.form}>
                         <form method="POST" action={productAPI} >
