8350921484bac91fed93046d76a6ee7ccfb8e875
Abdallah Khalfan <108978947+heisabdallah@users.noreply.github.com>
2023-03-14 22:16:47 +0300

    Added Feedbacks page

---
+++ pages/feedbacks.js
@@ -0,0 +1,44 @@
+import styles from "../styles/components/productsComp.module.scss"
+import { useState, useEffect } from "react";
+import Loading from "components/loading"
+
+function Feedbacks() {
+
+    const feedbackAPI = "api/feedback"
+    const [feedbacks, setFeedbacks] = useState([])
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchFeedbacks = async () => {
+            const response = await fetch(feedbackAPI)
+            const data = await response.json()
+
+            setFeedbacks(data)
+            setLoading(false);
+            
+        }
+        fetchFeedbacks()
+    }, [])
+
+
+    return (
+        <>
+            <div className={styles.feedbacks}>
+                {loading ? (
+                    <div className={styles.loading}><Loading /></div>
+                    ) : (
+                    feedbacks.map(feedback => (
+                        <div key={feedback._id} className={styles.data}>
+                            <p><b>Name:</b> {feedback.name}</p>
+                            <p><b>Make:</b> {feedback.message}</p>
+                            <p><b>Model:</b> {feedback.email}</p>
+                        </div>
+                    ))
+                )}
+            </div>
+        </>
+        
+    );
+}
+
+export default Feedbacks;
